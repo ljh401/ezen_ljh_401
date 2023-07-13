61c8f987e9b46cabf0b567f6b5c3390e3b237938
504 <504@DESKTOP-GQTNTS2>
2023-07-13 17:28:44 +0900

    Merge branch 'main' of https://github.com/ljh401/ezen_ljh_401 into main

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,32 @@
+package 과제.과제1;
+
+public class 과제1 {
+
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+
+	1. 4
+	2. 2,3,6,7
+	3. 정수 타입 순서대로 : byte , short/char , int , long
+	   실수 타입 순서대로 : float, double
+	   논리 타입 : boolean
+	   
+	4. 타입 : int, double
+	   변수 이름 : age, price
+	   리터럴 : 10, 3.14
+	5. 3 (short는 +-32000 이고 char는 0~65000 이므로 형변환X)
+	6. 4
+	7. 6
+	8. 3
+	9. 1
+	10. 2
+	11. 13,16
+
+	
+*/
+
+ 
