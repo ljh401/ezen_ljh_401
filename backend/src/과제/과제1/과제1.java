0b311ebf455ccf797b2b61bba1289d3d71848c5e
504 <504@DESKTOP-GQTNTS2>
2023-07-17 18:01:58 +0900

    da

--- backend/src/과제/과제1/과제1.java
+++ backend/src/과제/과제1/과제1.java
@@ -1,5 +1,7 @@
 package 과제.과제1;
 
+import java.util.Scanner;
+
 public class 과제1 {
 
 	public static void main(String[] args) {
@@ -47,7 +49,22 @@ public class 과제1 {
 		 * // 11. 변수의 허용범위 : {} 안에서 선언된 변수는 밖으로 호출/사용 불가
 		 * 
 		 */
-
+		
+		
+		
+		
+		//정수 하나를 입력 받아서 /2했을때 그 값을 반올림하여 출력하시오. 단,라이브러리 금지
+        Scanner sc = new Scanner(System.in);
+        float a =sc.nextFloat();
+        
+        //여기서부터//
+        float b = a/2;
+        int d = (int)b;
+        float c = (10*b)%10 >= 5 ? d+1 : d;
+        //여기까지 수정 가능
+        
+        System.out.println(c);
+        
 	}
 }
 
